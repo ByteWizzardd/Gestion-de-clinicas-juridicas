3ec9fad0876192186e1bfd3a018c0b716033638d
wildeswt <mfsandoval.23@est.ucab.edu.ve>
2025-11-21 14:38:55 -0400

    feat: componente de input

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import Button from "@/components/ui/Button"
-
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background">
---
+++ components/ui/Input.tsx
@@ -0,0 +1,14 @@
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    label?: string;
+    error?: string;
+}
+
+export default function Input({ label, error, ...props }: InputProps){
+    return(
+        <div className="flex flex-col gap-2">
+            {label && <label className="text-md font-medium">{label}</label>}
+            <input className="w-full p-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-primary" {...props} />
+            {error && <p className="text-md text-red-500">{error}</p>}
+        </div>
+    );
+}
