e68a5130d5b9e58e29ed09682ef7e58141cc6885
wildeswt <mfsandoval.23@est.ucab.edu.ve>
2025-11-21 14:35:50 -0400

    feat: componente de botón y colores en tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+'use client';
+
+import Button from "@/components/ui/Button"
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-background">
       <h1 className="text-4xl font-bold">oa muchachos ayuda</h1>
     </div>
   );
---
+++ components/ui/Button.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode;
+    variant?: 'primary' | 'secondary' | 'outline' | 'danger' | 'success';
+    size?: 'sm' | 'md' | 'lg';
+    isLoading?: boolean;    
+}
+
+export default function Button({ children, variant='primary', size='md', isLoading=false, disabled, ...props }: ButtonProps) {
+    //estilos base
+    const baseStyles = 'justify-center items-center inline-flex cursor-pointer rounded-md font-medium transition-colors disabled:opacity-50 disabled:pointer-events-none'
+    
+    //estilos para las variantes
+    const variantStyles = {
+        primary: "bg-primary text-white hover:bg-primary-dark",
+        secondary: "bg-secondary text-white hover:bg-secondary-dark",
+        outline: "bg-transparent text-primary border border-primary hover:bg-primary-light",
+        danger: "bg-danger text-white hover:bg-danger-dark",
+        success: "bg-success text-white hover:bg-success-dark"
+    }
+
+    //estilos para los tamaños
+    const sizeStyles = {
+        sm: "h-8 px-3 text-xs",
+        md: "h-10 px-4 py-2 text-sm",
+        lg: "h-12 px-6 text-base",
+    }
+    
+    return(
+        <button className={`${baseStyles} ${variantStyles[variant]} ${sizeStyles[size]}`} disabled={disabled} {...props}>
+            {isLoading ? <ArrowPathIcon className="animate-spin" /> : children}
+        </button>
+    );
+}
