a3fea6f2e0f8ec09c49226807921afd3556ffbc2
wildeswt <mfsandoval.23@est.ucab.edu.ve>
2026-01-18 17:34:26 -0400

    fix: erro de sintaxis enc ard de auditoria

--- components/audit/AuditEntityCard.tsx
+++ components/audit/AuditEntityCard.tsx
@@ -38,7 +38,7 @@ export default function AuditEntityCard({
       </h3>
 
       {/* Descripción */}
-      <p className="text-xs md:text-sm text-neutral-600 mb-3 relative z-10 flex-grow leading-relaxed">
+      <p className="text-xs md:text-sm text-neutral-600 mb-3 relative z-10 grow leading-relaxed">
         {description}
       </p>
 
