3672607ca3a97263213f75fdf2b535fb0384f376
Samuel Guzmán <samisak1805@gmail.com>
2026-02-25 10:14:01 -0400

    style: cambio del titulo de Auditoría

--- app/dashboard/audit/(main)/layout.tsx
+++ app/dashboard/audit/(main)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuditMainLayout({ children }: { children: ReactNode }) {
     return (
         <div className="w-full">
             <div className="mb-4 md:mb-6 mt-4">
-                <h1 className="text-4xl m-3 font-semibold font-primary">Auditoría del Sistema</h1>
+                <h1 className="text-4xl m-3 font-semibold font-primary">Auditoría</h1>
                 <p className="mb-6 ml-3 text-gray-600">Registro completo de todas las acciones realizadas en el sistema</p>
             </div>
             <div className="px-0">
